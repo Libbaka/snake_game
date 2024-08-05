09cf4434183e468aaafbebdd930176c9b4001337
Libbaka <libbaka@gmail.com>
2024-08-05 18:32:44 +0200

    snake

--- snake_llm.py
+++ snake_llm.py
@@ -3,19 +3,19 @@ import random
 
 pygame.init()
 
-# Nastavení obrazovky
+
 dis_width = 600
 dis_height = 400
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Had (Snake)')
 
-# Barvy
+
 black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-# Nastavení
+
 clock = pygame.time.Clock()
 snake_block = 10
 snake_speed = 15
@@ -69,7 +69,7 @@ def gameLoop_with_llm():
                 pygame.quit()
                 quit()
 
-        # Simulace state a tah LLM
+
         game_state = {'head': (x1, y1), 'food': (foodx, foody), 'snake': snake_List}
         move = get_llm_move(game_state)
 
@@ -86,11 +86,11 @@ def gameLoop_with_llm():
             y1_change = snake_block
             x1_change = 0
 
-        # Kontrola hranic
+
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_over = True
 
-        # Aktualizace pozice hada
+
         x1 += x1_change
         y1 += y1_change
         dis.fill(blue)
@@ -100,7 +100,7 @@ def gameLoop_with_llm():
         if len(snake_List) > Length_of_snake:
             del snake_List[0]
 
-        # Kontrola kolize s vlastním tělem
+
         for x in snake_List[:-1]:
             if x == snake_Head:
                 game_over = True
@@ -108,7 +108,6 @@ def gameLoop_with_llm():
         our_snake(snake_block, snake_List)
         pygame.display.update()
 
-        # Kontrola, zda had sežral jídlo
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
             foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
